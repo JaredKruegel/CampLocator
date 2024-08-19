8ed7f7e6102c393c08b4b913e1d0db8057006f54
JaredKruegel <92340185+JaredKruegel@users.noreply.github.com>
2024-08-19 16:12:37 -0400

    Add files via upload

---
+++ models/campground.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CampgroundSchema = new Schema({
+    title: String,
+    price: String,
+    descripton: String,
+    location: String
+})
+
+module.exports = mongoose.model('Campground', CampgroundSchema);
+
+
+
+
